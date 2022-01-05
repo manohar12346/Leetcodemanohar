d55fcc3b52f92473a75fff2246ecadbd6d832b84
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-01-05 19:59:56 +0530

    Time: 70 ms (10.91%), Space: 14.3 MB (72.18%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -5,7 +5,7 @@ class Solution:
         if len(nums) == 1 or nums[0] > nums[1]:
             return 0
         
-        # Ensure the last item is not the peak. This take care of input like so: [1, 2, 3, 4], [1, 2, 1, 6]
+        
         if nums[len(nums) - 1] > nums[len(nums) - 2]:
             return len(nums) - 1
         while(l<=h):
