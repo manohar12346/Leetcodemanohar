bcf5d744306d63e42339f51c40645e48b2996cc5
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-01-17 14:46:45 +0530

    Time: 40 ms (96.41%), Space: 14.4 MB (71.55%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -1,22 +1,39 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        x=[0]*(max(nums1)+1)
-        y=[0]*(max(nums2)+1)
-        z=min((max(nums1)+1),(max(nums2)+1))
-        for i in nums1:
-            x[i]+=1
-        for i in nums2:
-            y[i]+=1
-        ans=[]
-        for i in range(z):
-            if x[i]>0 and y[i]>0:
-                if min(x[i],y[i])>1:
-                    for j in range(min(x[i],y[i])):
-                        ans.append(i)
+        if len(nums1)>len(nums2):
+            
+            x={}
+            for i in nums2:
+                if i in x:
+                    x[i]+=1
                 else:
-                    ans.append(i)
-        return ans
+                    x[i]=1
+            ans=[]
+            for i in range(len(nums1)):
+                if nums1[i] in x:
+                    if x[nums1[i]]>0:
+                        ans.append(nums1[i])
+                        x[nums1[i]]-=1
+            
+                
                     
+                
+        else:
+            x={}
+            for i in nums1:
+                if i in x:
+                    x[i]+=1
+                else:
+                    x[i]=1
+            ans=[]
+            for i in range(len(nums2)):
+                if nums2[i] in x:
+                    if x[nums2[i]]>0:
+                        ans.append(nums2[i])
+                        x[nums2[i]]-=1
+        return ans           
+            
+      
         
         
         
