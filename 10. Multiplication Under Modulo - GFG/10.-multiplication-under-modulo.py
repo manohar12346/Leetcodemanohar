ac75d3082ea4d9558357e513f5a6254e63c22629
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-01-26 18:01:38 +0530

    Added solution - LeetHub

---
+++ 10. Multiplication Under Modulo - GFG/10.-multiplication-under-modulo.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+    def multiplicationUnderModulo(self,a,b):
+        '''
+        :param a: given value of a
+        :param b: given value of b
+        :return: Integer , sum under modulo
+        ''' 
+        return (a*b)%(10**9+7)
+        # code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        a,b = map(int,input().strip().split())
+        ob=Solution()
+        print(ob.multiplicationUnderModulo(a,b))
+
+# } Driver Code Ends
