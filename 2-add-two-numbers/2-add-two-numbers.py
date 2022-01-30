cdaef563b65844e22f8a5f9e19dfc1548b2a28e0
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-01-30 20:46:31 +0530

    Time: 72 ms (71.20%), Space: 14.1 MB (95.93%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,83 +5,73 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-            s=0
-            ans=ListNode()
-            head=ans
-            c=0
-            su=l1
-            u=l2
-            while(su.next or u.next):
-                new=ListNode(c,None)
-                ans.next=new
-                ans=new
-                if su.next:
-                    su=su.next
-                if u.next:
-                    u=u.next
-                c=c+1
-            so=0
+            b=0
+            new=ListNode()
+            d=new
             
-            
-                
-            an=head
-            va=-1
-            
-            while(head):
-                if l1 and l2:
-                    
-                    va=l1.val+l2.val+s
-                    if va>9:
-                        d=int(str(va)[1])
-                        s=int(str(va)[0])
-                        head.val=d
-                        
-
-
-                    else:
-                        s=0
-                        head.val=va
-                    l1=l1.next
-                    l2=l2.next
-                    head=head.next
-                elif l1:
-                    while(head and l1):
-                        va=l1.val+s
-                        if va>9:
-                            d=int(str(va)[1])
-                            s=int(str(va)[0])
-                            head.val=d
-                        else:
-                            s=0
-                            head.val=va
-                        head=head.next
-                        l1=l1.next
-                elif l2:
-                    while(head and l2):
-                        va=l2.val+s
-                        if va>9:
-                            d=int(str(va)[1])
-                            s=int(str(va)[0])
-                            head.val=d
-                        else:
-                            s=0
-                            head.val=va
-                            
-                        l2=l2.next
-                        head=head.next
-               
+            while(l1 and l2):
+                su=l1.val+l2.val+b
+                if su>9:
+                    b=int(str(su)[0])
+                    ne=ListNode()
+                    ne.val=int(str(su)[1])
+                    new.next=ne
+                    new=ne
                 else:
-                    break
-            if va>9:
-                f=int(str(va)[0])
-                last=ListNode(f,None)
-                head=an
-                while(head.next):
-                    head=head.next
-                head.next=last
+                    ne=ListNode()
+                    ne.val=su
+                    new.next=ne
+                    new=ne
+                    b=0
+                l1=l1.next
+                l2=l2.next
+            
+            if (l1):
+                print(l1.val)
+                while(l1):
+                    ne=ListNode()
+                    su=l1.val+b
+                    if su>9:
+                        b=int(str(su)[0])
+                        ne.val=int(str(su)[1])
+                        new.next=ne
+                        new=ne
+                    else:
+                        ne.val=su
+                        new.next=ne
+                        new=ne
+                        b=0
+                    l1=l1.next
+            else:
+                if (l2):
+                    while(l2):
+                        ne=ListNode()
 
+                        su=l2.val+b
+                        if su>9:
+                            b=int(str(su)[0])
+                            ne.val=int(str(su)[1])
+                            new.next=ne
+                            new=ne
+                        else:
+                            ne.val=su
+                            new.next=ne
+                            new=ne
+                            b=0
+                        l2=l2.next
+            print(b)
+            if b>0:
+                ne=ListNode()
+                ne.val=b
+                new.next=ne
+               
                 
-            return an
+                    
+                    
+            return d.next
+                    
+                    
+                    
                     
                             
                 
