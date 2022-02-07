de542cc82ebcd76e808c88838042764a276657a0
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-02-07 14:02:03 +0530

    Time: 144 ms (59.21%), Space: 19 MB (9.69%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,26 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        inte=sorted(intervals)
+        l,h=inte[0][0],inte[0][1]
+        ans=[]
+        a=0
+        print(inte)
+        for i in range(1,len(inte)):
+            if h>=inte[i][0]:
+                if h>inte[i][1]:
+                    h=h
+                else:
+                    h=inte[i][1]
+                a=0
+            else:
+                
+                a=1
+                ans.append([l,h])
+                l=inte[i][0]
+                h=inte[i][1]
+                
+       
+        ans.append([l,h])
+        return ans
+                
