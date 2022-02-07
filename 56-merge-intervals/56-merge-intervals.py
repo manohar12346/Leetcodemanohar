154372cd661de293e494461d45721a6e88c21440
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-02-07 14:03:07 +0530

    Time: 256 ms (16.30%), Space: 19 MB (9.69%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -12,10 +12,10 @@ class Solution:
                     h=h
                 else:
                     h=inte[i][1]
-                a=0
+            
             else:
                 
-                a=1
+                
                 ans.append([l,h])
                 l=inte[i][0]
                 h=inte[i][1]
