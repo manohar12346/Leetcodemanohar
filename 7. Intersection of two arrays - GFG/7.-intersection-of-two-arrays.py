63e4988b89c366b0ba36aff064680cbd2b23a736
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-03-01 16:48:24 +0530

    Added solution - LeetHub

---
+++ 7. Intersection of two arrays - GFG/7.-intersection-of-two-arrays.py
@@ -0,0 +1,60 @@
+#User function Template for python3
+
+#Function to return the count of the number of elements in
+#the intersection of two arrays.
+class Solution:
+    def NumberofElementsInIntersection(self,a, b, n, m):
+        #return: expected length of the intersection array.
+        x={}
+        for i in a:
+            x[i]=1
+        ans=0
+        for i in b:
+            if i in x:
+                if x[i]==1:
+                    
+                    ans+=1
+                x[i]+=1
+        return ans
+        
+        #code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob = Solution()
+        
+        print(ob.NumberofElementsInIntersection(a,b,n,m))
+        
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+                
+# } Driver Code Ends
