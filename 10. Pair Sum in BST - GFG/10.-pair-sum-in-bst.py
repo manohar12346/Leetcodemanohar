753a9b2b0fa1ad90ecc3211c241e4373c6d0768b
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-03-09 10:12:50 +0530

    Added solution - LeetHub

---
+++ 10. Pair Sum in BST - GFG/10.-pair-sum-in-bst.py
@@ -0,0 +1,102 @@
+#User function Template for python3
+
+
+#Function to check if any pair exists in BST whose sum is equal to given value.
+def findPair(root,X):
+    # code here
+    x=[]
+    def ino(root):
+        if root==None:
+            return
+        ino(root.left)
+        x.append(root.key)
+        ino(root.right)
+    ino(root)
+    
+    i=0
+    j=len(x)-1
+    while(i<j):
+        su=x[i]+x[j]
+        if su==X:
+            return True
+        elif su<X:
+            i=i+1
+        else:
+            j=j-1
+    return False
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+#Contributed by : Nagendra Jha
+
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.key = val
+        self.left = None
+        self.right = None
+
+hash = set() # hash to be used.
+# Tree Class
+class BSTree:
+    def __init__(self):
+        self.root = None
+
+    def Insert(self,x):
+        if self.root is None:
+            self.root = Node(x)
+            return
+        curr_node = self.root
+        while curr_node:
+            if curr_node.key < x: # go to right subtree if value is more
+                if curr_node.right is None:
+                    break
+                curr_node = curr_node.right
+            elif curr_node.key > x:   #  go to left subtree if value is more.
+                if curr_node.left is None:
+                    break
+                curr_node = curr_node.left
+            else:
+                return # no duplicate is to be inserted
+
+        # insert key at the leaf position.
+        if curr_node.key < x:
+            curr_node.right = Node(x)
+        else:
+            curr_node.left = Node(x)
+        return
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())  # number of nodes in tree
+        a = list(map(int, input().strip().split()))  # parent child info in list
+        x = int(input())
+        # construct the tree according to given list
+        tree = BSTree()
+        for value in a:
+            tree.Insert(value)  # Insert the nodes in tree.
+        if findPair(tree.root,x):
+            print(1)
+        else:
+            print(0)
+        hash = set()
+
+
+# } Driver Code Ends
