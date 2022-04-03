08ed46312191b5b3841775eed7254d268934ac86
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-04-04 00:15:06 +0530

    Time: 53 ms (47.82%), Space: 15.5 MB (58.12%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,15 +5,21 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        p=head
-        q=None
-        while(p):
-            r=q
-            q=p
-            p=p.next
-            q.next=r
-        head=q
+        # we need three varibles one for traversion in front(p) and one for current varibale(q) and last one for previous variable(r) so the present (q) adress should br changed to last one adress (r) at one hand we need to move p otherwise as we are changing the adreess the loop we will not move
+        
+        
+        prev=None
+    
+        pre=None
+        nex=head
+        while(nex):
+            prev=pre
+            pre=nex
+            nex=nex.next
+            pre.next=prev
+        head=pre
         return head
+    
         
             
         
