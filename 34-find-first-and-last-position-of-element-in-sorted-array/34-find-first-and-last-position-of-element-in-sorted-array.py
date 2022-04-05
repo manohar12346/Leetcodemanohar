fa87e9bf1e8e8042c4f05ced2e86282679bc2fc2
manohar12346 <67496574+manohar12346@users.noreply.github.com>
2022-04-05 15:42:30 +0530

    Time: 135 ms (32.91%), Space: 15.6 MB (12.20%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,40 +1,68 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l=0
-        h=len(nums)-1
-        p,po=-1,-1
-        while(l<=h):
-            mid=(l+h)//2
+#         l=0
+#         h=len(nums)-1
+#         p,po=-1,-1
+#         while(l<=h):
+#             mid=(l+h)//2
             
             
-            if nums[mid]==target:
-                p=mid
-                h=mid-1# here once we find the element try to check its left to find whetehr the same element is there in its left such we can get the element in the left most side
-            elif target<nums[mid]:
-                h=mid-1
-            elif target>nums[mid]:
-                l=mid+1
-        l=0
-        h=len(nums)-1
-        print(l,h)
-        while(l<=h):
-            mid=(l+h)//2
-            print(l,h)
+#             if nums[mid]==target:
+#                 p=mid
+#                 h=mid-1# here once we find the element try to check its left to find whetehr the same element is there in its left such we can get the element in the left most side
+#             elif target<nums[mid]:
+#                 h=mid-1
+#             elif target>nums[mid]:
+#                 l=mid+1
+#         l=0
+#         h=len(nums)-1
+#         print(l,h)
+#         while(l<=h):
+#             mid=(l+h)//2
+#             print(l,h)
             
             
-            if nums[mid]==target:
-                po=mid
-                l=mid+1# here once we find the element try to check its right to find whetehr the same element is there in its right such we can get the element in the right most side
-            elif target>nums[mid]:
-                l=mid+1
+#             if nums[mid]==target:
+#                 po=mid
+#                 l=mid+1# here once we find the element try to check its right to find whetehr the same element is there in its right such we can get the element in the right most side
+#             elif target>nums[mid]:
+#                 l=mid+1
                 
-            else:
+#             else:
                 
-                h=mid-1
-            
-                
-        return [p,po] 
+#                 h=mid-1
         
+            
+                
+#         return [p,po] 
+        l=0
+        h=len(nums)-1
+        mid=(l+h)//2
+        le=-1
+        while(l<=h):
+            mid=(l+h)//2
+            if nums[mid]>target:
+                h=mid-1
+            else:
+                if nums[mid]==target:
+                    le=mid
+                l=mid+1
+        l=0
+        h=len(nums)-1
+       
+        re=-1
+        while(l<=h):
+            mid=(l+h)//2
+            if nums[mid]<target:
+                l=mid+1
+            else:
+                if nums[mid]==target:
+                    re=mid
+                h=mid-1
+        return [re,le]
+        
+        
+    
        
             
         
