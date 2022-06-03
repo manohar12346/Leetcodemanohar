00c5448dc7c44f35d9e3d45ed3f97b217cc223ae
papudippumanohar123 <67496574+papudippumanohar123@users.noreply.github.com>
2022-06-03 10:13:22 +0530

    Time: 76 ms (28.78%), Space: 13.8 MB (85.89%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n==1:
+            return '1'
+        else:
+            no=["11"]
+            for i in range(1,n-1):
+                new=""
+                l=1
+
+                for j in range(len(no[0])-1):
+                    if no[0][j]==no[0][j+1]:
+                        l=l+1
+
+                    else:
+                        new+=str(l)+no[0][j]
+                        l=1
+                new+=str(l)+no[0][-1]
+                no[0]=new
+
+
+                
+            return no[0]
