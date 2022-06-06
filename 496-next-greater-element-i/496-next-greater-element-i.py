ef117f02481a829fe8cae4d83476ef42563bb0b6
papudippumanohar123 <67496574+papudippumanohar123@users.noreply.github.com>
2022-06-06 14:45:40 +0530

    Time: 55 ms (81.79%), Space: 14.1 MB (95.21%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,18 +1,33 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        x={}
+        s=[nums2[-1]]
+        ans=[0]*len(nums2)
+        ans[-1]=-1
+        dic={}
         for i in range(len(nums2)):
-            x[nums2[i]]=i
-        a=[]
-        for i in nums1:
-            c=x[i]
-            ans=-1
-            for j in range(c+1,len(nums2)):
-                if nums2[j]>i:
-                    ans=j
-                    a.append(nums2[j])
-                    break
-            if ans==-1:
-                a.append(-1)
-        return a
+            dic[nums2[i]]=i
+        for i in range(len(nums2)-2,-1,-1):
+            if nums2[i]<s[-1]:
+                ans[i]=s[-1]
+            else:
                 
+                while(len(s)>0 and nums2[i]>s[-1]):
+                    s.pop()
+               
+                if len(s)==0:
+                    
+                    ans[i]=-1
+                    
+                else:
+                    ans[i]=s[-1]
+            s.append(nums2[i])
+       
+        an=[]
+        for i in nums1:
+            an.append(ans[dic[i]])
+        return an
+            
+        print(ans)
+            
+            
+        
