a278db527197061a06774491b0c22f0b0d484439
papudippumanohar123 <67496574+papudippumanohar123@users.noreply.github.com>
2022-06-22 10:04:46 +0530

    Time: 487 ms (74.88%), Space: 21.9 MB (75.37%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -10,26 +10,26 @@ class Solution:
 #                 return max(sub(t,h,i-1,j),sub(t,h,i,j-1))
 #         return sub(text1,text2,len(text1)-1,len(text2)-1) Recursive Approach 
                     d=[]
-                    for i in range(len(text1)):
-                      d.append([0]*len(text2))
-                    for i in range(len(text1)):
-                        for j in range(len(text2)):
-                            if i==0 and j==0:
-                                if text1[i]==text2[j]:
-                                    d[i][j]=1
-                            if i==0:
-                                if text1[i]==text2[j]:
-                                    d[i][j]=1
-                                else:
-                                    d[i][j]=d[i][j-1]
-                            if j==0:
-                                if text1[i]==text2[j]:
-                                    d[i][j]=1
-                                else:
-                                    d[i][j]=d[i-1][j]
+                    for i in range(len(text1)+1):# + 1 is used to avoide base conditions like 0 th row and 0th column when we do -1
+                      d.append([0]*(len(text2)+1))
+                    for i in range(1,len(text1)+1):
+                        for j in range(1,len(text2)+1):
+#                             if i==0 and j==0:
+#                                 if text1[i]==text2[j]:
+#                                     d[i][j]=1
+#                             if i==0:
+#                                 if text1[i]==text2[j]:
+#                                     d[i][j]=1
+#                                 else:
+#                                     d[i][j]=d[i][j-1]
+#                             if j==0:
+#                                 if text1[i]==text2[j]:
+#                                     d[i][j]=1
+#                                 else:
+#                                     d[i][j]=d[i-1][j]
                                 
-                            else:
-                                if text1[i]==text2[j]:
+#                             else:
+                                if text1[i-1]==text2[j-1]:
                                     d[i][j]=1+d[i-1][j-1]
                                 else:
                                     d[i][j]=max(d[i-1][j],d[i][j-1])
