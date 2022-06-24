399a1b42a0ecedbbc2569a153d23f22f4e9368ab
papudippumanohar123 <67496574+papudippumanohar123@users.noreply.github.com>
2022-06-24 18:59:27 +0530

    Time: 795 ms (30.28%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select Department.name as "Department",Employee.name as "Employee",Employee.salary as"Salary" from Employee  join Department  on Employee.departmentId=Department.id where Employee.salary=(select max(salary) from Employee where Department.id=Employee.departmentId ) 
+select Department.name as "Department",Employee.name as "Employee",Employee.salary as"Salary" from Employee  join Department  on Employee.departmentId=Department.id   where Employee.salary=(select max(salary) from Employee where Department.id=Employee.departmentId ) 
